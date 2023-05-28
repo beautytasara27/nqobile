4178c01af578c2ae88fb19bd0d5bac7a56565952
beautytasara27 <beautytasara@gmail.com>
2023-05-28 10:18:53 +0200

    forms

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -54,6 +54,7 @@ const Contact = forwardRef((props, ref) => {
             <form
               className="flex flex-col sm:w-1/2 space-y-4"
               netlify
+              method="POST"
             >
               <input
                 name="Name"
